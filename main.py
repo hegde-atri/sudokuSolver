85ea17607db8e8044a2b96404f6dffad7babbc11
Atri Hegde <iamatrihegde@gmail.com>
2021-01-14 19:08:06 +0000

    initial to beta

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+from _tkinter import *
+import time
+from tkinter import *
+
+root = Tk()
+
+#Creation
+
+firstLabel = Label(root, text="First Label").grid(row=0, column=0)
+
+
+myButton = Button(root, text="Click me", padx=50, pady=50, state=DISABLED). grid(row=1, column=1)
+
+
+#run the window
+root.mainloop()
+
+
+
 board = [[8, 0, 0, 0, 0, 0, 0, 0, 0],
-				 [0, 0, 3, 6, 0, 0, 0, 0, 0],
+         [0, 0, 3, 6, 0, 0, 0, 0, 0],
          [0, 7, 0, 0, 9, 0, 2, 0, 0],
-				 [0, 5, 0, 0, 0, 7, 0, 0, 0],
+         [0, 5, 0, 0, 0, 7, 0, 0, 0],
          [0, 0, 0, 0, 4, 5, 7, 0, 0],
-				 [0, 0, 0, 1, 0, 0, 0, 3, 0],
+         [0, 0, 0, 1, 0, 0, 0, 3, 0],
          [0, 0, 1, 0, 0, 0, 0, 6, 8],
-				 [0, 0, 8, 5, 0, 0, 0, 1, 0],
+         [0, 0, 8, 5, 0, 0, 0, 1, 0],
          [0, 9, 0, 0, 0, 0, 4, 0, 0]]
 
 global counter
@@ -19,16 +38,17 @@ def solve(bo):
     else:
         row, col = find
 
-    for i in range(1,10):
+    for i in range(1, 10):
         if valid(bo, i, (row, col)):
             bo[row][col] = i
             print("Iteration number:\t" + str(counter))
+            #time.sleep(0.5)
             counter = counter + 1
             print_board(board)
             print("\n\n")
 
             if solve(bo):
-              return True
+                return True
 
             bo[row][col] = 0
 
@@ -50,9 +70,9 @@ def valid(bo, num, pos):
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
                 return False
 
     return True
@@ -81,10 +101,5 @@ def find_empty(bo):
 
     return None
 
-
-print_board(board)
-
-print("\n\n\n\n")
-solve(board)
 
 print_board(board)
